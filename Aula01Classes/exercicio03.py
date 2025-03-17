50d9baa10e332990b681075eb25636d2f66e3a5d
calouros <calouros@fatec.corp>
2025-03-17 10:58:54 -0300

    Arrumar Exer 3

---
+++ Aula01Classes/exercicio03.py
@@ -0,0 +1,18 @@
+#Exercício 3: Atributos de Instância
+
+#Tarefa: Crie uma classe Produto com os atributos nome e preco. Crie uma instância de Produto e modifique o atributo preco.
+#Objetivo: Compreender que atributos de instância são específicos de cada objeto.
+
+class Produto:
+    def __init__(self, nome_produto, preco_produto):
+        self.nome = nome_produto
+        self.preco = preco_produto
+
+    def exibir(self):
+        print(f"Nome do produto: {self.nome}\n Preço do produto: {self.preco}")
+    
+produto1 = Produto("Ovo de galinha", 12)
+produto1.exibir()
+
+produto1.preco(20)
+produto1.exibir()
